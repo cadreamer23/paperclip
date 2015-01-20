078b48841480651e553f2996a6903d1540c9eb6c
Dennis Charles Hackethal <dennis@generalassemb.ly>
2015-01-20 15:39:50 -0800

    Add paperclip solution

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ActiveRecord::Base
+	has_attached_file :preview, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :preview, :content_type => /\Aimage\/.*\Z/
+  validates_attachment :preview, size: { in: 0..120.megabytes }
 end
